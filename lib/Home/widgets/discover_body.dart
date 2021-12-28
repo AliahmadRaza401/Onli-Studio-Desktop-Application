88c7132c10c621b4d66050be41ffc00945955c87
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-12-29 00:14:08 +0500

    Hassan ~ discover screen modification

--- lib/Home/widgets/discover_body.dart
+++ lib/Home/widgets/discover_body.dart
@@ -17,118 +17,179 @@ class DiscoverBody extends StatelessWidget {
         horizontal: 10,
       ),
       color: myGreyDark,
-      child: Column(
-        children: [
-          //1st slider
-          Container(
-            // color: Colors.amber,
-            height: dynamicHeight(context, 0.4),
-            width: dynamicWidth(context, .7),
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              // shrinkWrap: true,
-              children: [
-                mainSliderBox(context, "assets/mainScrl1.png",),
-                mainSliderBox(context, "assets/mianScrl2.png",),
-                mainSliderBox(context, "assets/mianScrl3.png",),
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            //1st slider
+            SizedBox(
+              height: dynamicHeight(context, 0.32),
+              width: dynamicWidth(context, .7),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  mainSliderBox(
+                    context,
+                    "assets/mainScrl1.png",
+                  ),
+                  mainSliderBox(
+                    context,
+                    "assets/mianScrl2.png",
+                  ),
+                  mainSliderBox(
+                    context,
+                    "assets/mianScrl3.png",
+                  ),
+                ],
+              ),
             ),
-          ),
 
-          //2nd slider
-          Container(
-            // color: Colors.amber,
-            height: dynamicHeight(context, 0.13),
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              // shrinkWrap: true,
+            //2nd slider
+            SizedBox(
+              height: dynamicHeight(context, 0.13),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  secondSlider(
+                    context,
+                    "assets/miniScrl1.png",
+                    Color(0xff585855),
+                    Color(0xff585855),
+                  ),
+                  secondSlider(
+                    context,
+                    "assets/miniScrl2.png",
+                    Color(0xffF4C19B),
+                    Color(0xffEAEADC),
+                  ),
+                  secondSlider(
+                    context,
+                    "assets/miniScrl3.png",
+                    Color(0xff6D97EA),
+                    Color(0xff6D6D6D),
+                  ),
+                  secondSlider(
+                    context,
+                    "assets/miniScrl1.png",
+                    Color(0xff585855),
+                    Color(0xff585855),
+                  ),
+                  secondSlider(
+                    context,
+                    "assets/miniScrl2.png",
+                    Color(0xffF4C19B),
+                    Color(0xffEAEADC),
+                  ),
+                  secondSlider(
+                    context,
+                    "assets/miniScrl3.png",
+                    Color(0xff6D97EA),
+                    Color(0xff6D6D6D),
+                  ),
+                ],
+              ),
+            ),
+
+            // 3rd Slider
+
+            heightBox(context, .02),
+            Row(
               children: [
-
-                Container(
-                  height: dynamicHeight(context, 0.17),
-                  width: dynamicWidth(context, .23),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                        "assets/miniScrl1.png",
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Text(""),
-                ),
-                Container(
-                  height: dynamicHeight(context, 0.17),
-                  width: dynamicWidth(context, .23),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                        "assets/miniScrl2.png",
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Text(""),
-                ),
-                Container(
-                  height: dynamicHeight(context, 0.17),
-                  width: dynamicWidth(context, .23),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                        "assets/miniScrl3.png",
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Text(""),
-                ),
-                Container(
-                  height: dynamicHeight(context, 0.17),
-                  width: dynamicWidth(context, .23),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                        "assets/miniScrl1.png",
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Text(""),
-                ),
-                Container(
-                  height: dynamicHeight(context, 0.17),
-                  width: dynamicWidth(context, .23),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                        "assets/miniScrl2.png",
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Text(""),
-                ),
-                Container(
-                  height: dynamicHeight(context, 0.17),
-                  width: dynamicWidth(context, .23),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                        "assets/miniScrl3.png",
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Text(""),
+                text(
+                  context,
+                  "Articles",
+                  .014,
+                  Color(0xff6D6D6D),
                 ),
               ],
             ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-        ],
+
+            Divider(
+              color: Color(0xff2E2E2E),
+              height: dynamicHeight(context, .02),
+              thickness: 2.0,
+            ),
+
+            SizedBox(
+              height: dynamicHeight(context, 0.19),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  thirdSlider(
+                    context,
+                    "assets/OExperience.001.png",
+                    "About Onli",
+                  ),
+                  thirdSlider(
+                    context,
+                    "assets/building.png",
+                    "Onli ID",
+                  ),
+                  thirdSlider(
+                    context,
+                    "assets/miniScrl1.png",
+                    "Tutorial",
+                  ),
+                  thirdSlider(
+                    context,
+                    "assets/miniScrl1.png",
+                    "Presentation",
+                  ),
+                  thirdSlider(
+                    context,
+                    "assets/miniScrl1.png",
+                    "Blog",
+                  ),
+                  thirdSlider(
+                    context,
+                    "assets/nli.png",
+                    "Onli Build",
+                  ),
+                  thirdSlider(
+                    context,
+                    "assets/OExperience.001.png",
+                    "Onli Build",
+                  ),
+                ],
+              ),
+            ),
+
+            // 4th Slider
+
+            heightBox(context, .02),
+            Row(
+              children: [
+                text(
+                  context,
+                  "Community",
+                  .014,
+                  Color(0xff6D6D6D),
+                ),
+              ],
+            ),
+
+            Divider(
+              color: Color(0xff2E2E2E),
+              height: dynamicHeight(context, .02),
+              thickness: 2.0,
+            ),
+
+            SizedBox(
+              height: dynamicHeight(context, 0.17),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  fourthSlider(
+                    context,
+                    "assets/OExperience.001.png",
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -137,7 +198,7 @@ class DiscoverBody extends StatelessWidget {
 Widget mainSliderBox(context, image) {
   return Center(
     child: Container(
-      height: dynamicHeight(context, 0.4),
+      height: dynamicHeight(context, 0.32),
       width: dynamicWidth(context, .7),
       decoration: BoxDecoration(
         image: DecorationImage(
@@ -149,8 +210,8 @@ Widget mainSliderBox(context, image) {
       ),
       child: Padding(
         padding: EdgeInsets.only(
-          left: dynamicWidth(context, .02),
-          top: dynamicHeight(context, .26),
+          left: dynamicWidth(context, .012),
+          top: dynamicHeight(context, .2),
         ),
         child: Column(
           children: [
@@ -159,7 +220,7 @@ Widget mainSliderBox(context, image) {
                 text(
                   context,
                   "The Onli Presentation",
-                  .02,
+                  .014,
                   myDiscoverHeading,
                   boldText: FontWeight.w500,
                 ),
@@ -177,6 +238,180 @@ Widget mainSliderBox(context, image) {
             ),
           ],
         ),
+      ),
+    ),
+  );
+}
+
+Widget secondSlider(context, image, headingColor, subHeadingColor) {
+  return Padding(
+    padding: EdgeInsets.symmetric(
+      horizontal: dynamicWidth(context, .002),
+    ),
+    child: Container(
+      height: dynamicHeight(context, 0.13),
+      width: dynamicWidth(context, .23),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            image,
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: dynamicWidth(context, .004),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                text(
+                  context,
+                  "It’s Simpler Than You Think",
+                  .01,
+                  headingColor,
+                  boldText: FontWeight.w600,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                text(
+                  context,
+                  "Presentation",
+                  .008,
+                  subHeadingColor,
+                  boldText: FontWeight.w500,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget thirdSlider(context, image, title) {
+  return Padding(
+    padding: EdgeInsets.symmetric(
+      horizontal: dynamicWidth(context, .002),
+    ),
+    child: SizedBox(
+      height: dynamicHeight(context, 0.2),
+      width: dynamicWidth(context, .18),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: dynamicHeight(context, 0.12),
+            width: dynamicWidth(context, .23),
+            child: Image.asset(
+              image,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Spacer(),
+          Row(
+            children: [
+              text(
+                context,
+                title,
+                .008,
+                Color(0xff6F6F6F),
+                boldText: FontWeight.w600,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              text(
+                context,
+                "Author Name",
+                .008,
+                Color(0xff313131),
+                boldText: FontWeight.w600,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.star,
+                color: myWhite,
+                size: dynamicWidth(context, .006),
+              ),
+              Icon(
+                Icons.star,
+                color: myWhite,
+                size: dynamicWidth(context, .006),
+              ),
+              Icon(
+                Icons.star,
+                color: myWhite,
+                size: dynamicWidth(context, .006),
+              ),
+              Icon(
+                Icons.star,
+                color: myWhite,
+                size: dynamicWidth(context, .006),
+              ),
+              widthBox(context, .01),
+              text(context, "(23)", .006, Color(0xff313131))
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget fourthSlider(context, image) {
+  return Padding(
+    padding: EdgeInsets.symmetric(
+      horizontal: dynamicWidth(context, .002),
+    ),
+    child: SizedBox(
+      height: dynamicHeight(context, 0.2),
+      width: dynamicWidth(context, .14),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: dynamicHeight(context, 0.12),
+            width: dynamicWidth(context, .14),
+            child: Image.asset(
+              image,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Spacer(),
+          Row(
+            children: [
+              text(
+                context,
+                "title",
+                .008,
+                Color(0xff6F6F6F),
+                boldText: FontWeight.w600,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              text(
+                context,
+                "Author Name",
+                .008,
+                Color(0xff313131),
+                boldText: FontWeight.w600,
+              ),
+            ],
+          ),
+        ],
       ),
     ),
   );
