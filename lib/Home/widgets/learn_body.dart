8e30d88a1d6d0872e1a46a8ff78efc7f0a71de96
Your Name <you@example.com>
2021-12-29 01:35:04 +0500

    learn page

--- lib/Home/widgets/learn_body.dart
+++ lib/Home/widgets/learn_body.dart
@@ -112,72 +112,77 @@ class _LearnBodyState extends State<LearnBody> {
             ),
             // 2nd Coantianer
             Container(
-              height: dynamicHeight(context, .23),
+              height: dynamicHeight(context, .25),
               width: dynamicWidth(context, 1),
               color: Color(0xffD8D8D8),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          // horizontal: dynamicWidth(context, 0.03),
-                          vertical: dynamicHeight(context, 0.04),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: dynamicWidth(context, 0.02),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            // horizontal: dynamicWidth(context, 0.03),
+                            vertical: dynamicHeight(context, 0.04),
+                          ),
+                          width: dynamicWidth(context, 0.15),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              text(
+                                context,
+                                "Pinned Article & Featured",
+                                0.01,
+                                Color(0xff333333),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              text(
+                                context,
+                                "Author Dhryl Anton, Peter Haxel, Michael McFall",
+                                0.007,
+                                Color(0xffA9A9A9),
+                              ),
+                            ],
+                          ),
                         ),
-                        width: dynamicWidth(context, 0.2),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            text(
-                              context,
-                              "Pinned Article & Featured",
-                              0.01,
-                              Color(0xff333333),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            text(
-                              context,
-                              "Author Dhryl Anton, Peter Haxel, Michael McFall",
-                              0.007,
-                              Color(0xffA9A9A9),
-                            ),
-                          ],
+                        Container(
+                          width: dynamicWidth(context, 0.2),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              text(
+                                context,
+                                "Onli is a protocol. A protocol, in computer science, is a set of rules or procedures that governs the transfer of data between two or more electronic devices. A protocol establishes how the information must be structured and how each party is going to store it, send it and receive it.",
+                                0.006,
+                                Color(0xff333333),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      Container(
-                        width: dynamicWidth(context, 0.2),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            text(
-                              context,
-                              "Onli is a protocol. A protocol, in computer science, is a set of rules or procedures that governs the transfer of data between two or more electronic devices. A protocol establishes how the information must be structured and how each party is going to store it, send it and receive it.",
-                              0.006,
-                              Color(0xff333333),
-                            ),
-                          ],
+                        Container(
+                          width: dynamicWidth(context, 0.16),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              text(
+                                context,
+                                "Onli is a protocol. A protocol, in computer science, is a set of rules or procedures that governs the transfer of data between two or more electronic devices. A protocol establishes how the information must be structured and how each party is going to store it, send it and receive it.",
+                                0.006,
+                                Color(0xff333333),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      Container(
-                        width: dynamicWidth(context, 0.16),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            text(
-                              context,
-                              "Onli is a protocol. A protocol, in computer science, is a set of rules or procedures that governs the transfer of data between two or more electronic devices. A protocol establishes how the information must be structured and how each party is going to store it, send it and receive it.",
-                              0.006,
-                              Color(0xff333333),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                   Container(
                     child: Column(
@@ -232,6 +237,9 @@ class _LearnBodyState extends State<LearnBody> {
                         ),
                       ],
                     ),
+                  ),
+                  SizedBox(
+                    height: 5,
                   ),
                 ],
               ),
